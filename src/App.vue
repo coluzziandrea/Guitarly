cec96847a19e8c2120747848614be99475fdc6bb
Coluzzi Andrea <coluzziandrea.dev@gmail.com>
2022-05-05 00:32:51 +0200

    added tailwind

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,7 @@
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: "Roboto", sans-serif;
 }
 
 nav {
