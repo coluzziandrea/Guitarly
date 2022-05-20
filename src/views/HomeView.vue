d1428066fc70a66e283b72500418972cceb83f80
Andrea Coluzzi <coluzziandrea.dev@gmail.com>
2022-05-20 17:52:34 +0200

    wip Home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,22 @@
 <template>
-  <Tablature />
+  <div
+    class="flex-grow h-screen flex flex-col items-center justify-start mt-14 gap-10"
+  >
+    <h1
+      class="font-medium leading-7 tracking-wide text-5xl mt-0 mb-2 text-pistachio-600 drop-shadow-md"
+    >
+      Guitarly
+    </h1>
+    <h4 class="font-normal leading-5 text-xl text-gray-400">
+      Music Notation Software. Made Easy.
+    </h4>
+    <button
+      @click="openNewSheetModal"
+      class="p-5 bg-pistachio-500 rounded-full text-white text-center drop-shadow-md"
+    >
+      Create New Sheet
+    </button>
+  </div>
 </template>
 
 <script lang="ts">
@@ -8,9 +25,11 @@ import { Options, Vue } from "vue-class-component";
 import Tablature from "@/components/tablature/Tablature.vue";
 
 @Options({
-  components: {
-    Tablature,
-  },
+  components: {},
 })
-export default class HomeView extends Vue {}
+export default class HomeView extends Vue {
+  openNewSheetModal() {
+    this.$emit("open-new-sheet", {});
+  }
+}
 </script>
