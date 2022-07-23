9d9930ead488abc7fe6ce2e9d34523626fd89ecc
Coluzzi Andrea <coluzziandrea.dev@gmail.com>
2022-07-23 16:00:30 +0200

    wip dock

--- src/components/dock/Dock.vue
+++ src/components/dock/Dock.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="z-50 w-full bg-pistachio shadow-md h-[15%] px-8 py-4 text-white">
+  <div
+    class="z-50 w-full bg-pistachio shadow-md h-[15%] px-8 py-4 text-white select-none"
+  >
     <ul>
       <li v-for="track in sheetStore.tracks" v-bind:key="track.id">
-        <div class="grid"></div>
+        <div class="grid grid-cols-[repeat(4,1fr)_4fr]">
+          <span></span>
+          <span>{{ track.name }}</span>
+        </div>
       </li>
     </ul>
   </div>
